04d9115c081651b9afdc1d2b453800a5cbf52cc0
Goulshat <76894691+Goulshat@users.noreply.github.com>
2021-03-11 23:07:23 +0500

    правки
    переделала наоборот - по тех.заданию

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 const buttonFormDisplay = document.querySelector(".form-display-button");
 const searchForm = document.querySelector(".search-form-wrap");
 
-searchForm.classList.remove("search-form-open");
+searchForm.classList.add("search-form-closed");
 
 buttonFormDisplay.addEventListener("click", function (evt) {
   evt.preventDefault
-  searchForm.classList.toggle("search-form-open");
+  searchForm.classList.toggle("search-form-closed");
 });
